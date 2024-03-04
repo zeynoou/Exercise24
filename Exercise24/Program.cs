efbd66f08c5bee19d06f35248ae7e8775f134ba3
Zeynab <Zeynab@ZEYNAB>
2024-03-04 10:55:15 +0330

    Add project files.

---
+++ Exercise24/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise24
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please write somthing:");
+            string a = Console.ReadLine();
+            var result = FindWords(a);
+            Console.WriteLine(result);
+        }
+
+        public static string FindWords(string a)
+        {
+            if (a.Contains("bomb") || a.Contains("BOMB") || a.Contains("Bomb"))
+                return "Duck";
+            else
+                return "There is no bomb, Relax";
+        }
+    }
+
+}
